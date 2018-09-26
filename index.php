c5d5af5afdc0911afe9a64b23eacf36dbd23bf48
jonathanemiranda <jonathanmiranda93@gmail.com>
2018-09-26 09:04:08 -0600

    created and styled main content of index.php

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php include("inc/header.php");?>
 					
+<h1 class="pageHeadingBig">You Might Also Like</h1>
+<div class="gridViewConatiner">
+	<?php
+		$albumQuery = mysqli_query($con, "SELECT * FROM albums ORDER BY RAND() LIMIT 3");
+		while ($row = mysqli_fetch_array($albumQuery)) {
+			echo "<div class='gridViewItem'>
+					<a href='album.php?id=" .$row['id'] . "'>
+						<img src='" . $row['artwork_path'] . "'>
+						<div class='gridViewInfo'>"
+							. $row['title'] .
+						"</div>
+					</a>
+				</div>
+			";
+
+
+		}
+	?>
+</div>
 
 
 <?php include("inc/footer.php");?>
